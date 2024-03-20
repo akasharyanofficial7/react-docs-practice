c3ccad9f3e04ab74a38e7cf8ee9d79493b91d90f
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-03-20 06:05:21 +0530

    counter1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ export default function App() {
     <div>
       <h1>Counters that update together</h1>
       <MyButton count={count} onClick={handleClick} />
-      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
